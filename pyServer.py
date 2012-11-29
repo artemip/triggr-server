2b4147b3a2784d9f6133d7298c4915107e0e1cb2
Artem Pasyechnyk <artem.i.p@gmail.com>
2012-11-29 08:00:31 +0000

    Tornado + Redis

--- pyServer.py
+++ pyServer.py
@@ -1,62 +1,52 @@
-import string, cgi, time
-from os import curdir, sep
-from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
-from socket import *
+import threading
+import tornado.httpserver
+import tornado.websocket
+import tornado.ioloop
+import tornado.web
+import redis
 
-HOST = ""
-HTTP_SERVER_PORT = 8000
-SOCKET_PORT = 9090
-HTTP_ADDR = (HOST, HTTP_SERVER_PORT)
-SOCKET_ADDR = (HOST, SOCKET_PORT)
-BUFFER_SIZE = 256
+DEVICE_SOCKETS = dict()
 
-server = None
-socket_server = socket(AF_INET,SOCK_STREAM)
-socket_connection = None
-socket_address = None
+def redis_listener():
+    r = redis.Redis(host='localhost', db=2)
+    pubsub = r.pubsub()
+    pubsub.subscribe('new_request')
+    for request in pubsub.listen():
+        DEVICE_SOCKETS[request['device_id']].write_message(unicode(request['event']))
 
-class RequestHandler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        try:
-            if self.path.endswith("incoming_call"):
-                #Send data via socket to indicate 'mute' action
-                socket_connection.send("incoming_call")
-        
-                self.send_response(200)
-                self.send_header('Content-type', 'text/html')
-                self.end_headers()
-                self.wfile.write("Accepted IncomingCall Status")
-                return
-            elif self.path.endswith("call_ended"):
-                #Send data via socket to indicate 'un-mute' action
-                socket_connection.send("call_ended")
+class NewRequestHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.write("OK")
 
-                self.send_response(200)
-                self.send_header('Content-type', 'text/html')
-                self.end_headers()
-                self.wfile.write("Accepted CallEnded Status")
-                return
-        except IOError:
-            self.send_error(404, 'File Not Found: %s' % file_name)
+    def post(self):
+        data = self.request.arguments
+        r = redis.Redis(host='localhost', db=2)
+        pubsub = r.pubsub()
+        pubsub.publish('new_request', data)
 
-def main():
-    global socket_connection, socket_address, server
+class NewDeviceHandler(tornado.websocket.WebSocketHandler):
+    def open(self):
+        pass #DEVICE_SOCKETS[""] = ""
 
-    try:
-        socket_server.bind(SOCKET_ADDR)
-        socket_server.listen(5)
-        print "Socket listening..."
+    def on_message(self, message):
+        if(message.startswith("device_id:")):
+            self.device_id = message[10:]
+            DEVICE_SOCKETS[self.device_id] = self
+    
+    def on_close(self):
+        pass
 
-        socket_connection, socket_address = socket_server.accept()
-        print "Socket connected!"
+settings = {
+    'auto_reload': True,
+}
 
-        server = HTTPServer(('', 8000), RequestHandler)
-        print "Starting HTTPServer on port 8000"
-        server.serve_forever()
-    except KeyboardInterrupt:
-        print '^C received. Shutting down...'
-        socket_connection.close()
-        server.socket.close()
+application = tornado.web.Application([
+        (r'/', NewRequestHandler),
+        (r'/add_device', NewDeviceHandler),
+], **settings)
 
-if __name__ == '__main__':
-    main()
+if __name__ == "__main__":
+    threading.Thread(target=redis_listener).start()
+    http_server = tornado.httpserver.HTTPServer(application)
+    http_server.listen(8000)
+    tornado.ioloop.IOLoop.instance().start()
