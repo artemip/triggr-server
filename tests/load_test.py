39c98317fd2fb5716d6f571a7ca6b9a34e1cbd48
Artem Pasyechnyk <artem.i.p@gmail.com>
2013-10-05 19:15:36 +0000

    Improved load test

--- tests/load_test.py
+++ tests/load_test.py
@@ -4,6 +4,7 @@ import select
 import socket
 from timeit import Timer
 import threading
+import json
 
 POST_HEADER = {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/plain"}
 
@@ -17,17 +18,20 @@ def get_execution_time(function, *args, **kwargs):
     return round(Timer(partial(function, *args, **kwargs))
                  .timeit(1), 6)
 
-class TestClient(threading.Thread):
+class TestServer(threading.Thread):
     def __init__(self, id, server_host, server_port):
-        super(TestClient, self).__init__()
+        super(TestServer, self).__init__()
+        self.id = id
         self.socket_endpoint = (server_host, server_port)
         self.socket = None
-        self.id = id
 
     def run(self):
         self.connect()
         self.recv()
         self.close()
+
+    def registration_json(self):
+        return json.dumps({"message_type":"register_device","message":"%s" % self.id})
 
     def connect(self):
         global connected_cond
@@ -37,7 +41,7 @@ class TestClient(threading.Thread):
             self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             self.socket.connect(self.socket_endpoint)
             self.socket.settimeout(2)
-            self.socket.send("device_id:{0}\r\n".format(self.id))
+            self.socket.send(self.registration_json() + "\r\n")
 
             connection_countdown -= 1
             if connection_countdown == 0:
@@ -59,16 +63,13 @@ class TestClient(threading.Thread):
             except socket.error as se:
                 print "Socket error(%s):%s" % (se.errno, se.strerror)
                 break
-        
-class LoadTester():
-    global event_counter
-    global run_test
 
-    def __init__(self, api_endpoint, server_host, server_port):
-        self.client_threads = []
+class TestClient(threading.Thread):
+    def __init__(self, id, api_endpoint, num_events):
+        super(TestClient, self).__init__()
+        self.id = id
         self.api_conn = httplib.HTTPConnection(api_endpoint)
-        self.server_host = server_host
-        self.server_port = server_port
+        self.num_events = num_events
 
     def __del__(self):
         self.api_conn.close()
@@ -77,29 +78,78 @@ class LoadTester():
         self.api_conn.request("POST", "/events", params, POST_HEADER)
         self.api_conn.getresponse().read()
 
-    def __start_clients(self, num_clients):
-        run_test = True
-        self.client_threads = []
+    def __get_params(self):
+        event_json = {
+            "sender_id":"%s" % self.id,
+            "type":"test_event",
+            "notification": {
+                "icon_uri":"test_uri",
+                "title":"Test",
+                "subtitle":"SGH-i745a",
+                "description":"test_description"},
+            "handlers":["notify"]
+        }
+        
+        return urllib.urlencode({
+            'event':json.dumps(event_json),
+            'device_id':self.id
+        })
 
-        for i in range(0, num_clients):
-            thread = TestClient(i, self.server_host, self.server_port)
+    def run(self):
+        for i in xrange(self.num_events):
+            self.__POST_event(self.__get_params())
+
+    def event_json(self):
+        return json.dumps({"message_type":"register_device","message":"%s" % self.id})
+
+class LoadTester():
+    global event_counter
+    global run_test
+
+    def __init__(self, api_endpoint, server_host, server_port):
+        self.client_threads = []
+        self.server_threads = []
+        self.server_host = server_host
+        self.server_port = server_port
+        self.api_endpoint = api_endpoint
+
+    def __start_servers(self, num_servers):
+        self.server_threads = []
+
+        for i in range(0, num_servers):
+            thread = TestServer(i, self.server_host, self.server_port)
+            self.server_threads.append(thread)
+
+        for thread in self.server_threads:
+            thread.start()
+
+    def __start_clients(self, num_clients, num_events):
+        self.client_threads = []
+        events_per_client = num_events / num_clients
+        extra_events = num_events % num_clients
+
+        t = TestClient(0, self.api_endpoint, events_per_client + extra_events)
+        self.client_threads.append(t)
+        
+        for i in range(1, num_clients):
+            thread = TestClient(i, self.api_endpoint, events_per_client)
             self.client_threads.append(thread)
 
         for thread in self.client_threads:
             thread.start()
-
-    def __get_params(self, id):
-        return urllib.urlencode({
-                'event':'test_event',
-                'device_id':id
-                })
 
     def __stop_clients(self):
         for thread in self.client_threads:
             thread.join()
             del(thread)
 
-    def test(self, num_clients, num_events):
+    def __stop_servers(self):
+        for thread in self.server_threads:
+            thread.join()
+            del(thread)
+
+
+    def test(self, num_devices, num_events):
         global run_test
         global event_counter
         global connected_cond
@@ -108,24 +158,24 @@ class LoadTester():
         run_test = True
         event_counter = 0
         connected_cond = threading.Condition()
-        connection_countdown = num_clients
+        connection_countdown = num_devices
         
         with connected_cond:
-            self.__start_clients(num_clients)
+            self.__start_servers(num_devices)
             if connection_countdown > 0:
                 connected_cond.wait() # Wait for clients to be connected
+
+        self.__start_clients(num_devices, num_events)
+        self.__stop_clients()
         
-        try:
-            for i in range(0, num_events):
-                self.__POST_event(self.__get_params(i % num_clients))
-        finally:
-            run_test = False
-            self.__stop_clients()
+        run_test = False
+
+        self.__stop_servers()
 
         if event_counter == num_events:
-            print "Test Succeeded: (Clients: {0}, Events: {1})".format(num_clients, num_events)
+            print "Test Succeeded: (Clients: {0}, Events: {1})".format(num_devices, num_events)
         else:
-            print "Test Failed: (Clients: {0}, Events: {1}). Events processed: {2}".format(num_clients, num_events, event_counter)
+            print "Test Failed: (Clients: {0}, Events: {1}). Events processed: {2}".format(num_devices, num_events, event_counter)
 
 def main():
     load_tester = LoadTester(
@@ -133,12 +183,12 @@ def main():
         server_host = "localhost",
         server_port = 9090)
 
-    num_clients = [10000] 
-    num_events = [10000]
+    num_devices = [10, 100, 1000] 
+    num_events = [100, 1000, 10000]
 
-    for c in num_clients:
+    for c in num_devices:
         for e in num_events:
-            print "Time taken: %ss" % get_execution_time(load_tester.test, num_clients = c, num_events = e)
+            print "Time taken: %ss" % get_execution_time(load_tester.test, num_devices = c, num_events = e)
 
 if __name__ == '__main__':
     main()
